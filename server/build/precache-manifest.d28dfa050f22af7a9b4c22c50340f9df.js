5240f0a2335ce691b41cfc84bdfa065056f9b4e9
mo7amedsakr <msakr456@gmail.com>
2020-04-23 04:54:13 +0200

    client

---
+++ server/build/precache-manifest.d28dfa050f22af7a9b4c22c50340f9df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08a69330195e75858c20880b08682845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c375f4d773faaaa562c",
+    "url": "/static/css/main.6d7cf968.chunk.css"
+  },
+  {
+    "revision": "54f22bfd0a89dd0b2ce0",
+    "url": "/static/js/2.8135c792.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.8135c792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c375f4d773faaaa562c",
+    "url": "/static/js/main.874dbfb7.chunk.js"
+  },
+  {
+    "revision": "27fead4c70fc38b0920e",
+    "url": "/static/js/runtime-main.a4b3455f.js"
+  },
+  {
+    "revision": "1dad94d16e01a57f5edd7cf2ffa5ed8b",
+    "url": "/static/media/jeffrey_000.1dad94d1.png"
+  }
+]);
